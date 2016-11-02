ad49ec094db4c93aab4f17f12339e73fcd7d5233
PiscesThankIT <wuqiong1518@gmail.com>
2016-11-02 22:30:55 +0800

    commit

---
+++ KnockoutTutorial/Block/Main.php
@@ -0,0 +1,12 @@
+<?php
+namespace Pulsestorm\KnockoutTutorial\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Main extends Template
+{
+	protected function _prepareLayout()
+	{
+	    
+	} 
+}
---
+++ KnockoutTutorial/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Pulsestorm_KnockoutTutorial',
+    __DIR__
+);
